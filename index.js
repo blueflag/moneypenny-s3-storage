7947a6b20c14f14b2c3a913c7e2ba32455ed6bb7
Paul Esson <paul.esson@blueflag.com.au>
2017-02-21 09:36:58 +1100

    Inital comment
    Commit files for s3-storage

---
+++ index.js
@@ -0,0 +1,122 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+function get(bucket: String, id: String){
+    return new Promise((resolve, reject) => {
+        s3.getObject({
+            Bucket: bucket,
+            Key: id
+        }
+        ,(err, data) => {
+            if(err){
+                return reject(err);
+            }
+            else resolve(JSON.parse(data));
+        });
+    })
+}
+
+function put(bucket: String, id: String, data){
+    return new Promise((resolve, reject) => {
+        s3.putObject({
+            Bucket: bucket,
+            Key: id,
+            Body: JSON.stringify(client)
+        }
+        ,(err, data) => {
+            if(err){
+                return reject(err);
+            }
+            else resolve(data);
+        });
+    })
+}
+
+function remove(bucket: String, id){
+    return new Promise((resolve, reject) => {
+        s3.deleteObject({
+            Bucket: bucket,
+            Key: id
+        }
+        ,(err, data) => {
+            if(err){
+                return reject(err);
+            }
+            else resolve();
+        });
+    })
+}
+
+/**
+ * @param prefix {String} prefix at the start of the collection.
+ *
+ */
+function tokenStore(prefix: String, bucket: String){
+    return{
+        save: function(refreshToken){
+            return Promise.resolve(refreshToken)
+                .then((refreshToken) => {
+                    return put(bucket, `${prefix}/client-user/${refreshToken.userId}-${refreshToken.clientId}`, refreshToken);
+                })
+                .then((refreshToken) => {
+                    return put(bucket, `${prefix}/token/${refreshToken.token}`, refreshToken);
+                });
+        },
+        fetchByToken: function(token){
+            return get(bucket, `${prefix}/token/${token}`);
+        },
+        removeByUserIdClientId: function(userId, clientId){
+            return get(bucket, `${prefix}/token/${userId}-${clientId}`)
+                .then((refreshToken)=> {
+                    return remove(bucket, `${prefix}/client-user/${refreshToken.userId}-${refreshToken.clientId}`)
+                    .then(()=> {
+                        return remove(bucket, `${prefix}/token/${refreshToken.token}`)
+                    })
+                });
+        },
+        removeByRefreshToken: function(token){
+            return get(bucket, `${prefix}/token/${token}`)
+                .then((refreshToken)=> {
+                    return remove(bucket, `${prefix}/client-user/${refreshToken.userId}-${refreshToken.clientId}`)
+                    .then(()=> {
+                        return remove(bucket, `${prefix}/token/${refreshToken.token}`)
+                    })
+                });
+        }
+    }
+}
+
+module.exports = function(options){
+    const bucket = options.bucket;
+    return {
+        clientStore: {
+            createClient: (client) => {
+                return put(bucket, `clients/${client.id}`, client)
+            },
+            fetchById: (clientId) => {
+                return get(bucket, `clients/${clientId}`);
+            }
+        },
+        codeStore: {
+            save: (oauth2Code) => {
+                return put(bucket, `codes/${oauth2Code.code}`, oauth2Code);
+            },
+            fetchByCode: (code) => {
+                return get(bucket, `codes/${code}`);
+            },
+            removeByCode: (code) => {
+                return remove(bucket, `codes/${code}`);
+            }
+        },
+        refreshTokenStore: tokenStore('refresh-token', bucket),
+        tokenStore: tokenStore('token', bucket),
+        userStore: {
+            save: (user) => {
+                return put(bucket, `users/${user.id}`, user)
+            },
+            fetchById: (userId) => {
+                return get(bucket, `user/${userId}`);
+            }
+        }
+    }
+}
